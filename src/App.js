619c51e6dd1e9224aebfdcdd6cecd46d6aad2200
aruuwwaaa <arunamuratbekovaa0912@gmail.com>
2024-01-22 12:44:05 +0600

    fixed third day

--- src/App.js
+++ src/App.js
@@ -2,71 +2,82 @@ import { useEffect, useState } from "react";
 import { TodoListBody } from "./components/TodoListBody/TodoListBody";
 import { TodoListHeader } from "./components/TodoListHeader/TodoListHeader";
 
-function App() { 
-  const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem("tasks")) || []);
+function App() {
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || []
+  );
   const [searchTasks, setSearchTasks] = useState([]);
 
   useEffect(() => {
     localStorage.setItem("tasks", JSON.stringify(tasks));
-  }, [tasks])
+  }, [tasks]);
 
   const addTaskHandler = (task) => {
-      setTasks([task, ...tasks]);
-  }
+    setTasks([task, ...tasks]);
+  };
 
   const doneChangeHandler = (id) => {
-    const doneTask = tasks.filter(task => task.id == id)[0];
+    const doneTask = tasks.filter((task) => task.id == id)[0];
     doneTask.isDone = !doneTask.isDone;
 
-    if (doneTask.isDone){
-      setTasks([...tasks.filter(task => task.id != id), doneTask]);
-    }else{
-      setTasks([...tasks.filter(task => task.id != id), doneTask].sort((a,b) => b.id - a.id));
+    if (doneTask.isDone) {
+      setTasks([...tasks.filter((task) => task.id != id), doneTask]);
+    } else {
+      setTasks(
+        [...tasks.filter((task) => task.id != id), doneTask].sort(
+          (a, b) => b.id - a.id
+        )
+      );
     }
-  }  
+  };
 
   const editChangeHandler = (id) => {
-    setTasks(tasks.map(task => {
-      if (task.id == id){
-        task.isEdit = !task.isEdit;
-      }
-      return task;
-    }))
-  }
+    setTasks(
+      tasks.map((task) => {
+        if (task.id == id) {
+          task.isEdit = !task.isEdit;
+        }
+        return task;
+      })
+    );
+  };
 
   const deleteTaskHandler = (id) => {
-    setTasks(tasks.filter(task => task.id != id));
-  }
+    setTasks(tasks.filter((task) => task.id != id));
+  };
 
   const editTextHandler = (id, newTitle) => {
-    setTasks(tasks.map(task => {
-      if (task.id == id){
-        task.title = newTitle;
-      }
-      return task;
-    }))
-  }
+    setTasks(
+      tasks.map((task) => {
+        if (task.id == id) {
+          task.title = newTitle;
+        }
+        return task;
+      })
+    );
+  };
 
   const searchTaskHandler = (title) => {
-    if (title.trim().length > 0){
-      setSearchTasks(tasks.filter(task => task.title.includes(title)));
-    }else{
+    if (title.trim().length > 0) {
+      setSearchTasks(tasks.filter((task) => task.title.includes(title)));
+    } else {
       setSearchTasks([]);
     }
-  }
+  };
 
-  console.log("end of the day")
+  console.log("end of the day");
   console.log("end of the second day");
+  console.log("end of the third day");
 
   return (
     <div className="App">
       <div>
-        <TodoListHeader 
-          addTask={addTaskHandler} 
-          searchTaskHandler={searchTaskHandler} 
+        <TodoListHeader
+          addTask={addTaskHandler}
+          searchTaskHandler={searchTaskHandler}
           searchTasks={searchTasks}
         />
-        <TodoListBody 
+        <TodoListBody
           tasks={searchTasks.length > 0 ? searchTasks : tasks}
           doneChangeHandler={doneChangeHandler}
           editChangeHandler={editChangeHandler}
